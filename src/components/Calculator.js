144efd3c8fe273a3302f727aadf66c72fe674d8e
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-03 11:55:22 -0300

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,50 @@
 import React from 'react';
 import '../styles/Calculator.css';
+import Calculate from '../logic/Calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState((state) => Calculate(state, event.target.textContent));
   }
 
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calculator-container">
-        <div className="display">0</div>
+        <div className="display">{next || total || 0}</div>
         <div className="buttons-container">
           <div className="buttons-left">
             <div className="actions">
-              <span>AC</span>
-              <span>+/-</span>
-              <span>%</span>
+              <button type="button" onClick={this.handleClick}>AC</button>
+              <button type="button" onClick={this.handleClick}>+/-</button>
+              <button type="button" onClick={this.handleClick}>%</button>
             </div>
             <div className="numpad">
-              <span>9</span>
-              <span>8</span>
-              <span>7</span>
-              <span>6</span>
-              <span>5</span>
-              <span>4</span>
-              <span>3</span>
-              <span>2</span>
-              <span>1</span>
-              <span className="num-zero">0</span>
-              <span>.</span>
+              <button type="button" onClick={this.handleClick}>9</button>
+              <button type="button" onClick={this.handleClick}>8</button>
+              <button type="button" onClick={this.handleClick}>7</button>
+              <button type="button" onClick={this.handleClick}>6</button>
+              <button type="button" onClick={this.handleClick}>5</button>
+              <button type="button" onClick={this.handleClick}>4</button>
+              <button type="button" onClick={this.handleClick}>3</button>
+              <button type="button" onClick={this.handleClick}>2</button>
+              <button type="button" onClick={this.handleClick}>1</button>
+              <button type="button" onClick={this.handleClick} className="num-zero">0</button>
+              <button type="button" onClick={this.handleClick}>.</button>
             </div>
           </div>
           <div className="mathoperators">
-            <span>÷</span>
-            <span>X</span>
-            <span>-</span>
-            <span>+</span>
-            <span>=</span>
+            <button type="button" onClick={this.handleClick}>÷</button>
+            <button type="button" onClick={this.handleClick}>x</button>
+            <button type="button" onClick={this.handleClick}>-</button>
+            <button type="button" onClick={this.handleClick}>+</button>
+            <button type="button" onClick={this.handleClick}>=</button>
           </div>
         </div>
       </div>
