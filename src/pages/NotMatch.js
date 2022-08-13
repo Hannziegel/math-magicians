f2fd5f078d16758fde634611778d0754569091c0
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-13 15:50:16 -0300

    Add pages

---
+++ src/pages/NotMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/Global.css';
+
+const NotMatch = () => (
+  <div className="container">
+    <h2>No match for this page</h2>
+  </div>
+);
+export default NotMatch;
