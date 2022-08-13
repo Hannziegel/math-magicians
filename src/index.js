51b60c1b007bd9bdfe0819d9d33edd993313f85e
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-13 15:50:36 -0300

    Update App.js and Index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/Global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
 
